e3d270a0fa672a7552bd994794c4b78f622ef3c6
kien <huagiakien123@gmail.com>
2026-08-05 22:47:46 +0700

    Coi lãi suất

---
+++ NiiePay/Controllers/LaiSuatController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NiiePay.Entities;
+
+namespace NiiePay.Controllers
+{
+    // Đường dẫn vẫn giữ nguyên như tài liệu yêu cầu: api/savings
+    [Route("api/savings")]
+    [ApiController]
+    public class LaiSuatController : ControllerBase // Tên class tiếng Việt, CÓ đuôi Controller
+    {
+        private readonly NiiePayContext _context;
+
+        public LaiSuatController(NiiePayContext context)
+        {
+            _context = context;
+        }
+
+        // Đường dẫn API đầy đủ sẽ là: GET /api/savings/rates
+        [HttpGet("rates")]
+        public async Task<IActionResult> LayDanhSachLaiSuat() // Tên hàm tiếng Việt
+        {
+            var rates = await _context.LaiSuatKyHan
+                .OrderBy(r => r.TermMonths)
+                .Select(r => new {
+                    termMonths = r.TermMonths,
+                    interestRate = r.InterestRate
+                })
+                .ToListAsync();
+
+            return Ok(rates);
+        }
+    }
+}
---
+++ NiiePay/Entities/LaiSuatKyHan.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NiiePay.Entities
+{
+    [Table("LaiSuatKyHan")]
+    public class LaiSuatKyHan
+    {
+        [Key]
+        public int TermMonths { get; set; }
+        public decimal InterestRate { get; set; }
+    }
+}
